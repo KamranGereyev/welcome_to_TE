0bb4cf0402cacf241e2526f4dffb31c09b04c9e9
KamranGereyev <81572504+KamranGereyev@users.noreply.github.com>
2025-11-27 23:13:20 +0400

    Refactor components to use memo and PureComponent

--- task_1/index.tsx
+++ task_1/index.tsx
@@ -1,47 +1,64 @@
-import { Component } from 'react';
+import React, { Component, memo } from 'react';
 
 type IUser = {
-    name: string
-    age: number
-}
+  name: string,
+  age: number,
+};
 
 type IProps = {
-    user: IUser
-}
+  user: IUser,
+};
 
-// functional component
-const FirstComponent = ({ name, age }: IUser) => (
+// ---------- функциональные компоненты ----------
+
+const FirstComponent = memo(({ name, age }: IUser) => (
+  <div>
+    my name is {name}, my age is {age}
+  </div>
+));
+
+// -------------------------------------------------
+
+const areUserPropsEqual = (prev: IProps, next: IProps) =>
+  prev.user.name === next.user.name && prev.user.age === next.user.age;
+
+const SecondComponent = memo(
+  ({ user: { name, age } }: IProps) => (
     <div>
-        my name is {name}, my age is {age}
+      my name is {name}, my age is {age}
     </div>
+  ),
+  areUserPropsEqual,
 );
 
-// functional component
-// Этот компонент является необязательным для выполнения задания, но продемонстрирует глубину знаний в React.
-const SecondComponent = ({ user: { name, age } }: IProps) => (
-    <div>
-        my name is {name}, my age is {age}
-    </div>
-);
+// ---------- классовые компоненты ----------
 
-// class component
-class ThirdComponent extends Component<IUser> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.name}, my age is {this.props.age}
-            </div>
-        )
-    }
+class ThirdComponent extends React.PureComponent<IUser> {
+  render() {
+    return (
+      <div>
+        my name is {this.props.name}, my age is {this.props.age}
+      </div>
+    );
+  }
 }
 
-// class component
+// -------------------------------------------
+
 class FourthComponent extends Component<IProps> {
-    render() {
-        return (
-            <div>
-                my name is {this.props.user.name}, my age is {this.props.user.age}
-            </div>
-        )
-    }
+  shouldComponentUpdate(nextProps: IProps) {
+    const prevUser = this.props.user;
+    const nextUser = nextProps.user;
+
+    return prevUser.name !== nextUser.name || prevUser.age !== nextUser.age;
+  }
+
+  render() {
+    const { user } = this.props;
+    return (
+      <div>
+        my name is {user.name}, my age is {user.age}
+      </div>
+    );
+  }
 }
